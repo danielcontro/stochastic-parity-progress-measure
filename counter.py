aaa92d38e1cc5e3bf1a2b84a0cd82f7828d61786
Daniel Eduardo Contro <dani.contro2@gmail.com>
2024-07-06 19:52:31 +0200

    counter example fix

--- counter.py
+++ counter.py
@@ -15,35 +15,41 @@ from sympy import (
     zeros,
 )
 
-from sympy.logic.boolalg import Boolean
 from reactive_module import ReactiveModule
 from stochastic_parity_progress_measure import SPPM, ParityObjective
 
 # module M
-#   counter : [0..MAX_COUNTER] init MAX_COUNTER;
-#   processing : bool init false;
-#   [] processing = false -> 0.5 : (processing' = true) + 0.5 : (processing' = false);
-#   [] processing = true & counter > 0 -> 0.8 : counter_decr + 0.2 : reset;
-#   [] processing = true & counter > 0 -> 1 : reset;
-#   [] processing = true & counter = 0 -> 1 : reset;
+#   ticking  : bool init false;
+#   counter     : [0..MAX_COUNTER] init 0;
+#   q           : [0..1] init 0;
+#   [] ticking = false ->
+#       0.5 : (ticking' = true, counter' = MAX_COUNTER, q' = 1) +
+#       0.5 : (ticking' = false, counter' = 0, q' = 0);
+#   [] ticking = true & counter > 0 ->
+#       0.8 : (counter' = counter - 1) +
+#       0.2 : (ticking' = false, counter' = 0, q' = 0);
+#   [] ticking = true & counter > 0 ->
+#       1 : (ticking' = false, counter' = 0, q' = 0);
+#   [] ticking = true & counter = 0 ->
+#       1 : (ticking' = false, counter' = 0, q' = 0);
 # endmodule
 
 ticking = Symbol("ticking")
 counter = Symbol("counter")
+dpa = Symbol("q")
 MAX_COUNTER = 65536.0
-pvars = (ticking, counter)
+pvars = (ticking, counter, dpa)
 
 # Mutually exclusive guards as conjunction of inequalities/equalities
 not_ticking = Eq(ticking, 0)
 ticking_gt_0 = And(Eq(Add(ticking, -1), 0), StrictGreaterThan(counter, 0))
 ticking_eq_0 = And(Eq(Add(ticking, -1), 0), Eq(counter, 0))
 
-init = (0.0, MAX_COUNTER)
+init = (0.0, MAX_COUNTER, 0)
 
-wait_req = (eye(2), zeros(2, 1))
-to_proc = (Matrix([[0, 0], [0, 1]]), Matrix([[1], [0]]))
-reset = (zeros(2, 2), Matrix([[0.0], [MAX_COUNTER]]))
-counter_decr = (eye(2), Matrix([[0], [-1]]))
+to_proc = (zeros(3), Matrix([[1], [MAX_COUNTER], [1]]))
+reset = (zeros(3), Matrix([[0.0], [0], [0]]))
+counter_decr = (eye(3), Matrix([[0], [-1], [0]]))
 
 body = [
     # p = false -> 0.5 : to_proc; 0.5 : id
@@ -55,13 +61,12 @@ body = [
 ]
 
 # V_j in DNF form
-gf_waiting_requests: list[ParityObjective] = [
-    Eq(ticking, 0),
-    And(Eq(ticking - 1, 0), GreaterThan(counter, 0)),
-]
+gf_waiting_requests: list[ParityObjective] = [Eq(dpa, 0), Eq(Add(dpa, -1), 0)]
+
+q = [0, 1]
 
 rm = ReactiveModule(init, pvars, body)
-sppm = SPPM(rm, gf_waiting_requests)
+sppm = SPPM(rm, q, gf_waiting_requests)
 
 print("Starting synthesising")
 start_time = time()
